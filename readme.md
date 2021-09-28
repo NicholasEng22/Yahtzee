629bc13aec6d5c9fa87c097751061500fc2aeada
NicholasEng22 <61562438+NicholasEng22@users.noreply.github.com>
2021-09-28 19:36:40 -0400

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,11 @@
+# First Time- Install Dependencies
+
+`npm install`
+
+# Start Server
+
+`node server.js`
+
+# Request the website
+
+Visit <http://localhost:3000> in Chrome
---
+++ static/js/UI.js
@@ -0,0 +1,68 @@
+/* Variables */
+var rollsLeft = 3;
+document.getElementById('save-game').addEventListener('click', saveGame);
+document.getElementById('new-game').addEventListener('click', newGame);
+document.getElementById('load-game').addEventListener('click', loadGame);
+document.getElementById('roll-dice').addEventListener('click', rollDice);
+
+/* Functions  */
+
+function saveGame(){
+  //console.log(event.target);
+  console.log("Save game was clicked");
+  localStorage.setItem("Yahtzee", "Test");
+}
+
+function loadGame(){
+  //console.log(event.target);
+  console.log("Load game was clicked");
+  console.log(localStorage,getItem("Yahtzee"));
+}
+
+function newGame(){
+  //console.log(event.target);
+  console.log("New game was clicked");
+}
+
+function rollDice(){
+  //console.log(event.target);
+  rollsleft = rollsLeft - 1;
+
+  let img = 'images/blank.svg';
+  console.log("Roll dice was clicked");
+  document.getElementById("die-0").setAttribute('src', randDice());
+  document.getElementById("die-1").setAttribute('src', randDice());
+  document.getElementById("die-2").setAttribute('src', randDice());
+  document.getElementById("die-3").setAttribute('src', randDice());
+  document.getElementById("die-4").setAttribute('src', randDice());
+
+
+  rollsleft = rollsLeft - 1;
+  document.getElementById("rolls-remaining").innerHTML = rollsleft;
+}
+
+function randDice(){
+  let a = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
+  let b = 'images/blank.svg';
+
+  if (a == 0) {
+    b = 'images/one.svg';
+  }
+  else if (a == 1) {
+    b = 'images/two.svg';
+  }
+  else if (a == 2) {
+    b = 'images/three.svg';
+  }
+  else if (a == 3) {
+    b = 'images/four.svg';
+  }
+  else if (a == 4) {
+    b = 'images/five.svg';
+  }
+  else {
+    b = 'images/six.svg';
+  }
+  console.log();
+  return b;
+}
