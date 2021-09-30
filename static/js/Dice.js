e366e5901ae919fbf4aaad459f1fa5b0f0c43edc
NicholasEng22 <61562438+NicholasEng22@users.noreply.github.com>
2021-09-29 21:34:57 -0400

    Add files via upload

---
+++ static/js/Dice.js
@@ -0,0 +1,24 @@
+
+class Dice {
+  constructor(diceLabels, diceArray, diceElements) {
+    this.diceLabels = diceLabels;
+    this.diceArray = diceArray;
+    this.diceElements = diceElements;
+  }
+  roll() {
+    return this.name;
+  }
+  reset() {
+    return this.name;
+  }
+  getDiceArray() {
+    return this.diceArray;
+  }
+  getDiceElements() {
+    return this.diceElements;
+  }
+
+  setDieFace(element, newValue) {
+    return;
+  }
+}
