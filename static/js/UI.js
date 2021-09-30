5aa941b10ce3ed34f396bbf12782c0cfd0a660e8
NicholasEng22 <61562438+NicholasEng22@users.noreply.github.com>
2021-09-29 21:34:19 -0400

    Update UI.js

--- static/js/UI.js
+++ static/js/UI.js
@@ -5,9 +5,14 @@
 * Project Description: Replicate a game of Yahtzee
 */
 
+/* Imports */
+//import { dice } from 'Dice.js';
+
 /* Initialize Global Variables */
 let rollsLeft = 3;
 let diceArray = [0,0,0,0,0,0];
+let img = ['images/one', 'images/two', 'images/three', 'images/four', 'images/five', 'images/six'];
+let reserveBool;
 
 /* Add Event Listener */
 document.getElementById('save-game').addEventListener('click', saveGame);
@@ -15,21 +20,24 @@ document.getElementById('new-game').addEventListener('click', newGame);
 document.getElementById('load-game').addEventListener('click', loadGame);
 document.getElementById('roll-dice').addEventListener('click', rollDice);
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 5; i++) {
   document.getElementById('die-' + i).addEventListener('dblclick', reserveDie); //Add an event listener to each die
 }
 
-document.getElementById("rolls-remaining").innerHTML = rollsLeft;
+document.getElementById("rolls-remaining").innerHTML = rollsLeft; //Intializes the start roll
 
 /* Functions */
 function reserveDie(){
   console.log("Reserve toggled!");
-  event.target.classList.toggle("reserved")
+  event.target.classList.toggle("reserved");
 }
 
-function resetDice(){
+function resetDice(){ //Resets the dice to 0's
   rollsLeft = 3;
-  for (let i = 0; i < diceArray.length; i++) {
+  for (let i = 0; i < diceArray.length-1; i++) {
+    // if (document.getElementById("die-" + i).contains("reserved")) {
+    //   document.getElementById("die-" + i)classList.toggle("reserved"); //remove the reserved class
+    // }
     diceArray[i] = 0;
   }
 }
@@ -44,6 +52,9 @@ function loadGame(){
   //console.log(event.target);
   console.log("Load game was clicked");
   console.log(localStorage.getItem("Yahtzee"));
+  for (let i = 0; i < diceArray.length-1; i++) {
+    console.log(diceArray[i]);
+  }
 }
 
 function newGame(){
@@ -61,23 +72,17 @@ function rollDice(event){
   console.log(event.target);
   console.log("Roll dice was clicked");
 
-  let reserveBool = event.target.classList.contains("reserved");
+  reserveBool = event.target.classList.contains("reserved"); //Not working
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < diceArray.length-1; i++) {
     console.log(reserveBool);
     if (!reserveBool) {
-      document.getElementById("die-" + i).setAttribute('src', randDice() + ".svg");
-      
+      let a = Math.floor(Math.random() * (5 - 0 + 1)) + 0; //generates the random numbers
+      document.getElementById("die-" + i).setAttribute('src', img[a] + ".svg");
+      diceArray[i] = a+1;
     }
   }
 
   rollsLeft--;
   document.getElementById("rolls-remaining").innerHTML = rollsLeft;
-}
-
-function randDice(){
-  let a = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
-  let img = ['images/one', 'images/two', 'images/three', 'images/four', 'images/five', 'images/six'];
-
-  return img[a];
 }
