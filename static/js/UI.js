d50cf41b6859bf1169ff2f4c13bdff1254cc9c27
NicholasEng22 <61562438+NicholasEng22@users.noreply.github.com>
2021-09-30 08:10:09 -0400

    Update UI.js

--- static/js/UI.js
+++ static/js/UI.js
@@ -5,7 +5,7 @@
 * Project Description: Replicate a game of Yahtzee
 */
 
-/* Imports */
+/* Connect Dice class to UI */
 //import { dice } from 'Dice.js';
 
 /* Initialize Global Variables */
@@ -72,9 +72,11 @@ function rollDice(event){
   console.log(event.target);
   console.log("Roll dice was clicked");
 
-  reserveBool = event.target.classList.contains("reserved"); //Not working
+
 
   for (let i = 0; i < diceArray.length-1; i++) {
+    rollingDie = document.getElementById("die-" + i);
+    reserveBool = rollingDie.classList.contains("reserved"); //Not working
     console.log(reserveBool);
     if (!reserveBool) {
       let a = Math.floor(Math.random() * (5 - 0 + 1)) + 0; //generates the random numbers
